104ddaab52a404600cf1e165f20bbd3071314dd7
M_Kachi <mkachi@naver.com>
2016-07-13 11:53:31 +0900

    Add python class embedding Add sample Add error message Type override (PyModule, PyFunction, PyVar, PyArgs) = PyObject*

--- PyCure/main.cpp
+++ PyCure/main.cpp
@@ -2,29 +2,111 @@
 
 using namespace PyCure;
 
+//PyObject* TestFunc(PyObject* self)
+//{
+//	PyObject* result;
+//	printf("Call Cpp TestFunc!\n");
+//	Py_INCREF(Py_None);
+//	result = Py_None;	// return 없음
+//	return result;
+//}
+//
+//PyObject* Multiple(PyObject* self, PyObject* args)
+//{
+//	PyObject* result;
+//	int a, b;
+//	Py_INCREF(args);
+//	PyArg_ParseTuple(args, "II", &a, &b);
+//	result = PyType(a * b);
+//	return result;
+//}
+//
+//// 함수를 등록
+//// METH_KEYWORDS	: 함수가 PyCFunctionWithKeywords 타입 인자는 PyArg_ParseTupleAndKeywords함수를 사용
+//// METH_NORARGS		: 인자가 없음
+//// METH_O			: 인자가 하나인 경우
+//// METH_CLASS		: 클래스의 메소드일 경우
+//// METH_STATIC		: 클래스의 메소드일 경우
+//static PyMethodDef ExtMethods[] =
+//{ 
+//	{ "TestFunc", (PyCFunction)TestFunc, METH_NOARGS, "Excute a TestFunc" },{ NULL, NULL, 0, NULL },
+//	{ "Multiple", (PyCFunction)Multiple, METH_VARARGS, "Excute a Multiple" },{ NULL, NULL, 0, NULL }
+//};
+//
+//// 모듈에 등록
+//static PyModuleDef PyExtModule =
+//{ PyModuleDef_HEAD_INIT, "PyCureDemo", NULL, -1, ExtMethods };
+//
+//PyMODINIT_FUNC PyInit_Ext1()
+//{
+//	// 모듈을 생성
+//	return PyModule_Create(&PyExtModule);
+//}
+
 int main()
 {
+//	PyImport_AppendInittab("PyCureDemo", PyInit_Ext1);
 	PyCure_Initialize();
 
-	PyModule module = nullptr;
-	module = PyCure_ImportModule("Sample01");
+	PyModule module = PyCure_ImportModule("Sample01");
 
-	// Call Case1()
+	int result;
+
+	// Get global variable 'o'
+	result = GetVar<int>(module, "o");
+	printf("Get o : %d\n", result);
+
+	// Set global variable 'o'
+	printf("\n");
+	SetVar(module, "o", 9999);
+	result = GetVar<int>(module, "o");
+	printf("Set o : %d\n", result);
+
+	// Call function 'Case1()'
+	printf("\n");
 	CallFunc<void>(module, "Case1");
 
-	// Call Case2()
-	int result = CallFunc<int>(module, "Case2");
+	// Call function 'Case2()'
+	printf("\n");
+	result = CallFunc<int>(module, "Case2");
 	printf("Case2 return : %d\n", result);
 
-	// Call Case3()
+	// Call function 'Case3(a)'
+	printf("\n");
 	PyTuple tuple = TupleCallFunc(module, "Case3", 2);
-	int num = GetTupleItem<int>(tuple, 0);
+	result = GetTupleItem<int>(tuple, 0);
 	const char* str = GetTupleItem<const char*>(tuple, 1);
-	printf("Case3 return %d, %s\n", num, str);
+	printf("Case3 return : %d, %s\n", result, str);
 
-	// Call Add
+	// Call function 'Add(a, b)'
+	printf("\n");
 	result = CallFunc<int>(module, "Add", PyCureArgs(2, PyType(1), PyType(1)));
-	printf("Add return %d\n", result);
+	printf("Add return : %d\n", result);
+
+	// Import Python class
+	PyClass Ang = CreatePyClass(module, "Ang");
+
+	// Get class Ang local variable '_a'
+	printf("\n");
+	result = Ang->GetLocalVar<int>("_a");
+	printf("Get class Ang _a : %d\n", result);
+
+	// Set class Ang local variable '_a'
+	printf("\n");
+	Ang->SetLocalVar("_a", 99);
+	result = Ang->GetLocalVar<int>("_a");
+	printf("Set class Ang _a : %d\n", result);
+
+	// Call class Ang Method 'GetA()'
+	printf("\n");
+	result = Ang->CallMethod<int>("GetA");
+	printf("class Ang GetA return : %d\n", result);
+
+	// Call class Ang Method 'SetA(a)'
+	printf("\n");
+	Ang->CallMethod<void>("SetA", PyCureArgs(1, PyType(100)));
+	result = Ang->CallMethod<int>("GetA");
+	printf("class Ang SetA : %d\n", result);
 
 	PyCure_Finalize();
 	return 0;
--- include/PyCure.h
+++ include/PyCure.h
@@ -7,31 +7,54 @@
 
 namespace PyCure
 {
+	using PyModule = PyObject*;
+	using PyFunction = PyObject*;
+	using PyVar = PyObject*;
+	using PyArgs = PyObject*;
+
 	namespace internal
 	{
-		static std::list<PyObject*> PyCureModules;
+		static std::list<PyModule> __PyCureModules;
+		class PyCureModule
+		{
+		protected:
+			PyModule	_module;
 
+		public:
+			PyModule GetModule() { return _module; }
+
+		};
 		class _PyTuple
+			: public PyCureModule
 		{
 		private:
-			PyObject*	_core;
 			int			_count;
 			int			_counter;
 
 		public:
-			_PyTuple(PyObject* object, int count)
-				: _core(object), _count(count), _counter(0)
-			{	}
+			_PyTuple() = delete;
+
+			_PyTuple(PyModule module, int count)
+				: _count(count), _counter(0)
+			{
+				assert(module != NULL && "Unassigned module!");
+				_module = module;
+			}
+
+			_PyTuple(const _PyTuple& other)
+				: _count(other._count), _counter(other._counter)
+			{
+				_module = other._module;
+			}
 
 			~_PyTuple()
 			{
-				if (_core != NULL)
+				if (_module != NULL)
 				{
-					Py_DECREF(_core);
+					Py_DECREF(_module);
 				}
 			}
 
-			PyObject* GetData() { return _core; }
 			void Pull()
 			{
 				++_counter;
@@ -41,46 +64,92 @@ namespace PyCure
 				}
 			}
 		};
-	}
+		class _PyClass
+			: public PyCureModule
+		{
+		private:
+			const char* _className;
 
+		public:
+			_PyClass() = delete;
+
+			_PyClass(PyModule module, const char* className)
+				: _className(className)
+			{
+				_module = PyObject_CallMethod(module, _className, NULL);
+				assert(_module != NULL && "Class import Failed!");
+				internal::__PyCureModules.push_back(_module);
+			}
+
+			_PyClass(const _PyClass& other)
+				: _className(other._className)
+			{
+				_module = other._module;
+			}
+
+			~_PyClass()
+			{	}
+
+			template<typename T>
+			inline void SetLocalVar(const char* name, T value)
+			{
+				SetVar<T>(_module, name, value);
+			}
+
+			template<typename T>
+			inline T GetLocalVar(const char* name)
+			{
+				return GetVar<T>(_module, name);
+			}
+
+			template<typename T>
+			inline T CallMethod(const char* name, PyArgs args = nullptr) noexcept
+			{
+				return CallFunc<T>(_module, name, args);
+			}
+
+			const char* const GetClassName() { return _className; }
+
+		};
+	}
 	using PyTuple = internal::_PyTuple*;
-	using PyModule = PyObject*;
+	using PyClass = internal::_PyClass*;
 
 	template<typename T>
-	inline T PyType(PyObject* value) noexcept
+	inline T PyType(PyVar value) noexcept
 	{	}
-
 #define PyCure_PyType_PyToCpp(_TYPE_, _ARG_) \
 	template<> \
-	inline _TYPE_ PyType<_TYPE_>(PyObject* value) noexcept \
+	inline _TYPE_ PyType<_TYPE_>(PyVar value) noexcept \
 	{ \
 		_TYPE_ result; \
+		assert(value != NULL && "Unassigned variable!"); \
 		PyArg_Parse(value, _ARG_, &result); \
 		Py_DECREF(value); \
 		return result; \
 	}
 
 	template<typename T>
-	inline PyObject* PyType(T value) noexcept
+	inline PyVar PyType(T value) noexcept
 	{	}
-
 #define PyCure_PyType_CppToPy(_TYPE_, _ARG_) \
 	template<> \
-	inline PyObject* PyType(_TYPE_ value) noexcept \
+	inline PyVar PyType(_TYPE_ value) noexcept \
 	{ \
-		return Py_BuildValue(_ARG_, value); \
+		PyVar result = Py_BuildValue(_ARG_, value); \
+		assert(result!=NULL && "Value Build Failed!"); \
+		return result; \
 	}
 
 	template<typename T>
 	inline T GetTupleItem(PyTuple tuple, unsigned int index) noexcept
 	{	}
-
 #define PyCure_GetTupleItem(_TYPE_, _ARG_) \
 	template<> \
 	inline _TYPE_ GetTupleItem<_TYPE_>(PyTuple tuple, unsigned int index) noexcept \
 	{ \
 		_TYPE_ result; \
-		PyArg_Parse(PyTuple_GetItem(tuple->GetData(), index), _ARG_, &result); \
+		PyArg_Parse(PyTuple_GetItem(tuple->GetModule(), index), _ARG_, &result); \
 		tuple->Pull(); \
 		return result; \
 	}
@@ -107,22 +176,36 @@ namespace PyCure
 	PyCure_GetTupleItem(float, "f");
 
 	template<typename T>
+	inline void SetVar(PyModule module, const char* name, T value) noexcept
+	{
+		PyVar var = PyType(value);
+		PyObject_SetAttrString(module, name, var);
+	}
+
+	template<typename T>
 	inline T GetVar(PyModule module, const char* name) noexcept
 	{
-		PyObject* var = PyObject_GetAttrString(module, name);
+		PyVar var = PyObject_GetAttrString(module, name);
+		assert(var != NULL && "Variable import failed");
 		return PyType<T>(var);
 	}
 
 	template<typename T>
-	inline T CallFunc(PyModule module, const char* name, PyObject* args = nullptr) noexcept
+	inline T CallFunc(PyModule module, const char* name, PyArgs args = nullptr) noexcept
 	{
 		T result;
-		PyObject* function = PyObject_GetAttrString(module, name);
-		PyObject* pyResult = PyObject_CallObject(function, args);
-		if (pyResult != Py_None)
+		PyFunction function = PyObject_GetAttrString(module, name);
+		assert(function != NULL && "Function import failed");
+		PyVar pyResult = PyObject_CallObject(function, args);
+		if (pyResult == Py_None || pyResult == NULL)
+		{
+			assert(!"This function has no return value.");
+		}
+		else
 		{
 			result = PyType<T>(pyResult);
 		}
+
 		if (args != nullptr)
 		{
 			Py_DECREF(args);
@@ -133,28 +216,35 @@ namespace PyCure
 	}
 
 	template<>
-	inline void CallFunc<void>(PyModule module, const char* name, PyObject* args) noexcept
+	inline void CallFunc<void>(PyModule module, const char* name, PyVar args) noexcept
 	{
-		PyObject* function = PyObject_GetAttrString(module, name);
-		PyObject* pyResult = PyObject_CallObject(function, args);
+		PyFunction function = PyObject_GetAttrString(module, name);
+		assert(function != NULL && "Function import failed");
+		PyVar pyResult = PyObject_CallObject(function, args);
 		if (args != nullptr)
 		{
 			Py_DECREF(args);
 		}
-		Py_DECREF(pyResult);
+		if (pyResult != NULL)
+		{
+			Py_DECREF(pyResult);
+		}
 		Py_DECREF(function);
 	}
 
-	inline PyTuple TupleCallFunc(PyModule module, const char* name, unsigned int dataNum, PyObject* args = nullptr) noexcept
+	inline PyTuple TupleCallFunc(PyModule module, const char* name, unsigned int tupleCount, PyArgs args = nullptr) noexcept
 	{
-		PyObject* function = PyObject_GetAttrString(module, name);
-		PyObject* pyResult = PyObject_CallObject(function, args);
+		assert(tupleCount >= 0 && "tupleCount is to small");
+
+		PyFunction function = PyObject_GetAttrString(module, name);
+		assert(function != NULL && "Function import failed");
+		PyVar pyResult = PyObject_CallObject(function, args);
 		if (args != nullptr)
 		{
 			Py_DECREF(args);
 		}
 		Py_DECREF(function);
-		return new internal::_PyTuple(pyResult, dataNum);
+		return new internal::_PyTuple(pyResult, tupleCount);
 	}
 
 	inline void PyCure_Initialize()
@@ -164,21 +254,60 @@ namespace PyCure
 
 	inline void PyCure_Finalize()
 	{
-		for (auto iter = internal::PyCureModules.begin(); iter != internal::PyCureModules.end(); ++iter)
+		for (auto iter = internal::__PyCureModules.begin(); iter != internal::__PyCureModules.end(); ++iter)
 		{
 			if ((*iter) != NULL)
 			{
 				Py_DECREF((*iter));
 			}
 		}
-		internal::PyCureModules.clear();
+		internal::__PyCureModules.clear();
 		Py_Finalize();
 	}
 
 	inline PyModule PyCure_ImportModule(const char* name)
 	{
 		PyModule result = PyImport_ImportModule(name);
-		internal::PyCureModules.push_back(result);
+		if (result == NULL)
+		{
+			assert(!"Module import Failed!");
+		}
+		internal::__PyCureModules.push_back(result);
+		return result;
+	}
+
+	inline void ClearModule(PyModule module)
+	{
+		if (module != NULL)
+		{
+			internal::__PyCureModules.erase(
+				std::find(internal::__PyCureModules.cbegin(), internal::__PyCureModules.cend(), module));
+			Py_DECREF(module);
+		}
+		else
+		{
+			assert(!"Unassigned module!");
+		}
+	}
+
+	inline void ClearModule(PyClass pyClass)
+	{
+		PyModule module = pyClass->GetModule();
+		if (module != NULL)
+		{
+			internal::__PyCureModules.erase(
+				std::find(internal::__PyCureModules.cbegin(), internal::__PyCureModules.cend(), module));
+			Py_DECREF(module);
+		}
+		else
+		{
+			assert(!"Unassigned module!");
+		}
+	}
+
+	inline PyClass CreatePyClass(PyModule module, const char* className)
+	{
+		PyClass result = new internal::_PyClass(module, className);
 		return result;
 	}
 }
