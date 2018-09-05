c5de080aff35e6a56607af2c77f54bb0a24d0d6d
MKachi <mkachi@naver.com>
2018-09-05 23:01:11 +0900

    Add Sample Scripts

---
+++ Scripts/Embedding.py
@@ -0,0 +1,25 @@
+sample = 100
+
+class SampleClass:
+	_a = 99
+	
+	def getA(self):
+		print("Call SampleClass getA()")
+		return self._a;
+
+	def setA(self, a):
+		print("Call SampleClass setA()")
+		self._a = a;
+		return
+
+def function1():
+	print("Call function1()")
+	return
+
+def function2():
+	print("Call function2() has return value 1")
+	return 1
+
+def function3():
+	print("Call function3() has return value 2")
+	return 1, "Test"
--- include/PyCure.hpp
+++ include/PyCure.hpp
@@ -1,1 +1,11 @@
 #pragma once
+
+#include <Python.h>
+
+namespace PyCure
+{
+	typedef PyObject*	PyModule;
+	typedef PyObject*	PyFunction;
+	typedef PyObject*	PyVar;
+	typedef PyObject*	PyArgs;
+}
